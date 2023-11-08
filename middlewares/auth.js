dfaae3a7bf1be1e838ad934d7bc6f2f71bf09295
Muhammad Ravi <muhammadravi1127@gmail.com>
2023-11-09 00:54:19 +0700

    add migrate user, photo and comment & make auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,45 @@
+const { verifyToken } = require("../helpers/jwt");
+const { user } = require("../models");
+
+const authentication = async (req, res, next) => {
+  try {
+    const token = req.header("token");
+    // cek header token
+    if (!token) {
+      throw {
+        code: 401,
+        message: "Token not provided!",
+      };
+    }
+
+    // verify token
+    const decode = verifyToken(token);
+
+    const userData = await user.findOne({
+      where: {
+        id: decode.id,
+        email: decode.email,
+        username: decode.username,
+      },
+    });
+
+    if (!userData) {
+      throw {
+        code: 401,
+        message: "User not found!",
+      };
+    }
+
+    req.userData = {
+      id: userData.id,
+      email: userData.email,
+      username: userData.username,
+    };
+
+    next();
+  } catch (error) {
+    res.status(error.code || 500).json({ message: error.message });
+  }
+};
+
+module.exports = authentication;
---
+++ migrations/20231108174213-create-comment.js
@@ -0,0 +1,61 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Comments", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+      },
+      PhotoId: {
+        type: Sequelize.INTEGER,
+      },
+      comment: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.addConstraint("Comments", {
+      fields: ["UserId"],
+      type: "foreign key",
+      name: "user_fk_comments",
+      references: {
+        table: "Users",
+        field: "id",
+      },
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    });
+
+    await queryInterface.addConstraint("Comments", {
+      fields: ["PhotoId"],
+      type: "foreign key",
+      name: "photo_fk_comments",
+      references: {
+        table: "Photos",
+        field: "id",
+      },
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("Comments", "user_fk_comments");
+    await queryInterface.removeConstraint("Comments", "photo_fk_comments");
+    await queryInterface.dropTable("Comments");
+  },
+};
