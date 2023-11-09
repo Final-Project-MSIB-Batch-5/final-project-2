f5eae985228b665a79714821411bcbb4b2c098d8
Gaffur10 <adegaffur@gmail.com>
2023-11-09 22:37:35 +0800

    test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const routes = require("./routers");
+const routes = require("./routers/route");
+const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,4 +11,7 @@ app.get("/", (req, res) => {
   res.send("Final Project 2 Kelompok 10");
 });
 
+app.listen(PORT, () => {
+  console.log('running on PORT ${PORT}')
+})
 module.exports = app;
